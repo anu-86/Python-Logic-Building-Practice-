b5f85ea8a0aa56861f5635c536106f3e6ae5350a
Anushiya V <anu866786@gmail.com>
2026-06-17 18:00:26 +0530

    Create Palindrome Number.py

---
+++ Palindrome Number.py
@@ -0,0 +1,13 @@
+# palindrome Number
+
+n = int(input())
+original = n
+reverse = 0
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n = n // 10
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
